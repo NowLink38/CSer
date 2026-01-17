da3ffc2f5cfcfaa5339374217cbde947753be90f
NowLink38 <nowlink38@gmail.com>
2026-01-17 23:01:05 +0000

    Update server.py

--- server.py
+++ server.py
@@ -27,7 +27,6 @@ def audio_html(radio_name):
 @app.route("/")
 def index():
     return """
-    <h2>Radio Recordings</h2>
     <ul>
         <li><a href="/capb">capb</a></li>
         <li><a href="/hrtw">hrtw</a></li>
